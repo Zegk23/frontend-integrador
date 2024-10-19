eee4b73774dc4df11d8e386eba7219bca3efcdbd
Luis <lmartinezquijandria@gmail.com>
2024-10-19 12:34:58 -0500

    Avance 1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+
+export default function HomePage() {
+  return (
+    <>
+    <div className="container mt-5 bg-danger">
+        <h1 className="text-center text-light">Home Page</h1>
+    </div>
+    </>
+)
+}
